1c851ea8b03509e531abd9ff4f9fc9c090014398
Dean <dean.murphy90@gmail.com>
2018-02-06 21:20:48 -0800

    mocks made functions written need to test

--- lab-dean/__test__/lib/mocks.js
+++ lab-dean/__test__/lib/mocks.js
@@ -1,18 +1,57 @@
 'use strict';
 
+const User = require('../../model/auth');// vinicio - similar to a user
 const faker = require('faker');
-const Auth = require('../../model/auth.js');
+const Gallery = require('../../model/gallery');
 
 const mock = module.exports = {};
 
-mock.auth = {};
+mock.user = {};
 
-mock.auth.createOne = () => {
-  return new Auth({
+mock.user.createOne = () => {
+  let result = {};
+  result.password = faker.internet.password();
+
+  let user = new User({
     username: faker.internet.userName(),
     email: faker.internet.email(),
-    password: faker.internet.password,
   });
+
+  return user.generatePasswordHash(result.password)
+    .then(auth => {
+      result.auth = auth;
+      return auth.save();
+    })
+    .then(auth => auth.generateToken())
+    .then(token => {
+      result.token = token;
+      return result;
+    });
 };
 
-mock.auth.removeAll = () => Promise.all([Auth.remove()]);
+mock.user.removeAll = () => Promise.all([User.remove()]);
+
+
+mock.gallery = {};
+
+mock.gallery.createOne = () => {
+  let resultMock = null;
+
+  return mock.user.createOne()
+    .then(createdUserMock => resultMock = createdUserMock)
+    .then(createdUserMock => {
+      return new Gallery({
+        name: faker.internet.domainWord(),
+        description: faker.random.words(15),
+        userId: createdUserMock.auth._id,
+      }).save();
+    })
+    .then(gallery => {
+    
+      resultMock.gallery = gallery;
+      return resultMock;
+    });
+};
+
+
+mock.gallery.removeAll = () => Promise.all([Gallery.remove()]);
--- lab-dean/route/route-gallery.js
+++ lab-dean/route/route-gallery.js
@@ -5,30 +5,61 @@ const bodyParser = require('body-parser').json();
 const errorHandler = require('../lib/error-handler.js');
 const bearerAuthMiddleware = require('../lib/bearer-auth-middleware.js');
 
-module.exports = router => {
-  router.route('/gallery/:id?')
-    .post(bearerAuthMiddleware, bodyParser, (request, response) => {
+const ERROR_MESSAGE = 'Authorization Failed';
 
+module.exports = router => {
+  
+  router.route('/gallery/:_id?')
+    .post(bearerAuthMiddleware,bodyParser,(request,response) => {
       request.body.userId = request.user._id;
 
       return new Gallery(request.body).save()
         .then(createdGallery => response.status(201).json(createdGallery))
-        .catch(err => errorHandler(err, res));
+        .catch(error => errorHandler(error,response));
     })
 
-    .get(bearerAuthMiddleware,(req, res) => {
-      if(req.params._id) {
-        return Gallery.findById(req.params._id)
-          .then(gallery => res.status(200).json(gallery))
-          .catch(err => errorHandler(err, res));
+    .get(bearerAuthMiddleware,(request,response) => {
+      if(request.params._id){
+        return Gallery.findById(request.params._id)
+          .then(gallery => response.status(200).json(gallery))
+          .catch(error => errorHandler(error,response));
       }
 
       return Gallery.find()
         .then(galleries => {
           let galleriesIds = galleries.map(gallery => gallery._id);
 
-          res.status(200).json(galleriesIds);
+          response.status(200).json(galleriesIds);
         })
-        .catch(err => errorHandler(err, res));
+        .catch(error => errorHandler(error,response));
+    })
+    .put(bearerAuthMiddleware, bodyParser, (request,response) => {
+      Gallery.findById(request.params._id,request.body)
+        .then(gallery => {
+          if(gallery.userId === request.user._id) {
+            gallery.name = request.body.name || gallery.name;
+            gallery.description = request.body.description || gallery.description;
+            return gallery.save();
+          }
+          if (request.body.name === undefined || request.body.description === undefined ) {
+            throw new Error('validation');
+          }
+        
+          return new Error('validation');
+        })
+        .then(() => response.sendStatus(204))
+        .catch(error => errorHandler(error,response));
+    })
+
+    .delete(bearerAuthMiddleware,(request,response) => {
+      return Gallery.findById(request.params._id)
+        .then(gallery => {
+          if(gallery.userId.toString() === request.user._id.toString())
+            return gallery.remove();
+          
+          return errorHandler(new Error(ERROR_MESSAGE),response);
+        })
+        .then(() => response.sendStatus(204))
+        .catch(error => errorHandler(error,response));
     });
 };
